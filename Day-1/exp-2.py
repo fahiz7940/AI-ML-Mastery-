d09aa9a51397b2f42546b9154dc35714506259b4
Fahiz Frontend dev <mfahiz99@gmail.com>
2025-12-16 19:32:16 +0530

    day-1.01

---
+++ Day-1/exp-2.py
@@ -0,0 +1,19 @@
+# String 
+x = str(3)
+print(x)
+print(type(x))
+
+#integer
+y = int(3)
+print(y)
+print(type(y))
+
+#float
+z = float(3)
+print(z)
+print(type(z))
+
+# Boolen
+s = bool(True)
+print(s)
+print(type(s))
