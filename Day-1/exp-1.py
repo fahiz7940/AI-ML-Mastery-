39963584aa2d6e4ee278ccdae9cd85cd696e6113
Fahiz Frontend dev <mfahiz99@gmail.com>
2025-12-16 19:17:05 +0530

    day-1

--- Day-1/exp-1.py
+++ Day-1/exp-1.py
@@ -1,2 +1,21 @@
-print("Hello, My name is Fahiz.")
-print("Im Starting my AI/ML journey")
+#this is how to print a String in Python
+# print("Hello, My name is Fahiz.")
+# print("Im Starting my AI/ML journey")
+
+#Variable:-
+
+#String
+myName = "Fahiz"
+food = "Noodels"
+email = "snnn@gmail.com"
+
+#Integer
+age = 22
+myHeight = 5.5
+
+print(f"Helo' {myName}")
+print(f"Your favorite Food is {food}")
+print(f"Your email ID is {email}")
+print(f"Yoyr age is {age}")
+print(f"Your Height is {myHeight}")
+
